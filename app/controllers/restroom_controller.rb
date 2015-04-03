a1eb63b3ed9e85cb5751191fcf4b1a2563418b1e
ikaruce <ikaruce@gmail.com>
2015-04-03 23:43:19 +0900

    censor calc command inssert

--- app/controllers/restroom_controller.rb
+++ app/controllers/restroom_controller.rb
@@ -5,7 +5,21 @@ class RestroomController < ApplicationController
   end
 
   def status
-    @status = "4124"
+    @status = report_status
     render json:{"id" => "2", "status" => @status } 
   end
+
+private 
+  def report_status
+    distance = system("sudo python c.py")
+    begin
+      if distance.to_f < 50
+        return "full"
+      else 
+        return "empty"
+    rescue
+      puts "ERROR OCCURRED -- distance : " + distance
+      return "FAIL"
+    end
+  end 
 end
